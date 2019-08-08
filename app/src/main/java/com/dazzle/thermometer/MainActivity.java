7b89bcee581031fd0cb97acf3efe17018e3db83b
zhuw <738504427@qq.com>
2019-08-08 17:00:44 +0800

    桌面左上角实现获取Cpu温度显示

--- app/src/main/java/com/dazzle/thermometer/MainActivity.java
+++ app/src/main/java/com/dazzle/thermometer/MainActivity.java
@@ -2,7 +2,14 @@ package com.dazzle.thermometer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
     private ThermometerView thermometerView;
@@ -11,7 +18,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        thermometerView =findViewById(R.id.view_temp);
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        try {
+            double fTemp = Double.parseDouble(getTemp(this));
+            double rT = fTemp / 1000;
+            DecimalFormat DEEE = new java.text.DecimalFormat("#.0");
+            String temp = DEEE.format(rT);
+            Log.e("zhuw", "temp =" +temp);
+            thermometerView.setTemp(this,temp);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
+    public String getTemp(Context context) throws IOException {
+        StringBuilder sb = new StringBuilder("");
+        FileInputStream inputStream = new FileInputStream(new File("/sys/class/thermal/thermal_zone0/temp"));
+        byte[] buffer = new byte[1024];
+        int len = inputStream.read(buffer);
+        while(len > 0){
+            sb.append(new String(buffer,0,len));
+            len = inputStream.read(buffer);
+        }
+        inputStream.close();
+        return sb.toString();
     }
 }
